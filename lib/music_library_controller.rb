ce88e6b2f9f488fe0aa67c2c4cc1682e688b4516
Ariel <arielord@buffalo.edu>
2019-01-08 19:52:43 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -91,7 +91,8 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.to_i
     
-    #list = self.list_songs
+    list = self.list_songs
+    
     #puts "Playing #{list[input.to_i-1].name} by #{list[input.to_i-1].artist.name}"
   end
 
